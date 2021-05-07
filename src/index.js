8bbb9cbfed0ecd99d95ca86454939cc4a5c82911
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-05-07 13:27:47 -0300

    criando projeto

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { StatusBar, SafeAreaView, View, Text } from 'react-native';
+
+
+function App () {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>OLÁ MUNDO</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+      
+export default App;
