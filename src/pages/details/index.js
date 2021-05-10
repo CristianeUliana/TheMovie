a83ec4e4bb4a8e35980acc2bffd067e8588e7e2a
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-05-10 16:20:24 -0300

    página de detalhes criada

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import {
+    Container,
+    PosterImage,
+    TextContainer,
+    TextTitle,
+    TextOriginalTilte,
+    TextOverviewTitle,
+    TextOverview,
+    TextRating,
+} from './styles'
 
 
 function Details () {
+
+
+
+    
     return(
-        <View />
+        <Container>
+            <PosterImage source={{uri: 'https://image.tmdb.org/t/p/w500/9yBVqNruk6Ykrwc32qrK2TIE5xw.jpg'}} />
+            <ScrollView>
+                <TextContainer>
+                    <TextTitle>NOME DO FILME</TextTitle>
+                    <TextOriginalTilte>Nome em inglês</TextOriginalTilte>
+                    <TextOverviewTitle>Sinopse:</TextOverviewTitle>
+                    <TextOverview>Ipsum loresum ahiucnve naiuhdiuf iuhcinjovn ushgiewbek nsbyugfiufnm ndiubcinjkcno duhbuifbinc  ibviubineo  vbybiheow ibefiubuohoiw ibubuiesvbono bfiubauibiub</TextOverview>
+                    <TextRating>Rating: 5.4</TextRating>
+                </TextContainer>
+            </ScrollView>
+        </Container>
     )
 }
 
--- src/pages/details/styles.js
+++ src/pages/details/styles.js
@@ -7,16 +7,50 @@ export const Container = styled.SafeAreaView`
 `;
 
 export const PosterImage = styled.Image`
-    width: auto;
-    height: 30%px;
+    width: 100%;
+    height: 35%;
 `;
 
 export const TextContainer = styled.View`
-    background-color: red;
     margin-top: 20px;
-    min-width: 90%;
-    min-height: 8%;
-    border-radius: 8px;
-    align-items: center;
-    justify-content: center;
+    width: 90%;
+    height: auto;
+`;
+
+export const TextTitle = styled.Text`
+    color: #F8F8FF;
+    font-size: 23px;
+    font-weight: 700;
+    letter-spacing: 3px;
+    margin-top: 10px
+`;
+
+export const TextOriginalTilte = styled.Text`
+    color: #777;
+    font-size: 14px;
+    font-style: italic;
+    margin-top: 10px
+`;
+
+
+export const TextOverviewTitle = styled.Text`
+    color: #F8F8FF;
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 30px
+`;
+
+export const TextOverview = styled.Text`
+    color: #777;
+    font-size: 19px;
+    margin-top: 10px;
+    text-align: justify;
+
+`;
+
+export const TextRating = styled.Text`
+    color: #F8F8FF;
+    font-size: 15px;
+    font-style: italic;
+    margin-top: 30px
 `;
